519681864e20c489afb4cefbabbf9a400e04e0c7
mozngige <exscett@gmail.com>
2023-11-20 16:30:00 +0300

    added deployed links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://book-store-mern-stack-w9kd.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
